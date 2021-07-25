8e974fd70f0caae779b7eed253ed4bc2f7b6a453
Dadamuga Mazino <dadamuga.mazino@zencargo.com>
2021-07-25 21:18:35 +0100

    added tests struts

---
+++ api/src/resolvers/spec/InsititutionResolver.spec.ts
@@ -0,0 +1,17 @@
+describe("Institution Resolver", () => {
+  it("creates an institution", async () => {
+    expect(response).toEqual(expectedResponse);
+  });
+
+  it("retrieves all institutions", async () => {
+    expect(response).toEqual(expectedResponse);
+  });
+
+  it("updates an institution", async () => {
+    expect(response).toEqual(expectedResponse);
+  });
+
+  it("deletes an institution", async () => {
+    expect(response).toEqual(expectedResponse);
+  });
+});
